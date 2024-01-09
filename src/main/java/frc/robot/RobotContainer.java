5bc83ccd083efe0817da9557cac2028f2a0a2507
ryanjc25 <25rchang@hinghamschools.org>
2024-01-09 16:11:40 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
+
 public class RobotContainer {
+  private CommandXboxController controller = new CommandXboxController(0);
+
+
   public RobotContainer() {
     configureBindings();
   }
