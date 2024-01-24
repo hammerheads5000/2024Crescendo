eea565977d634dea52d3d8e0660110cfc8ac537d
Tori <ndtech260@gmail.com>
2024-01-24 16:02:58 -0500

    actuators!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,28 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.ActuatorSubsystem;
 
 public class RobotContainer {
+  Servo actuatorServo = new Servo(1); // PWM channel 1
+  ActuatorSubsystem actuatorSubsystem = new ActuatorSubsystem(actuatorServo);
+  ActuateCommand actuateCommand = new ActuateCommand(actuatorSubsystem);
+
+  CommandXboxController controller = new CommandXboxController(0);
+  Trigger actuateTrigger = controller.a();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    actuateTrigger.whileTrue(actuateCommand);
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
