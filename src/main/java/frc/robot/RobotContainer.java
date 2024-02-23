015f19e38e6095d01a3843fb21b3a541c216251b
Tori <ndtech260@gmail.com>
2024-02-23 10:40:01 -0500

    test the lidar only

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,37 +20,13 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
-  TalonSRX rollers = new TalonSRX(12);
-  Servo linearActuator = new Servo(2);
-
-  CommandXboxController controller = new CommandXboxController(0);
-  Trigger intakeTrigger = controller.rightBumper();
-  Trigger expelTrigger = controller.leftBumper();
-  Trigger autoTrigger = controller.a();
-  Trigger extendTrigger = controller.x();
-  Trigger contractTrigger = controller.b();
   public DigitalInput lidar = new DigitalInput(4);
 
-  
-  Command intakeCommand = new FunctionalCommand(
-    () -> rollers.set(TalonSRXControlMode.PercentOutput, -0.5), 
-    () -> {}, 
-    (interrupted) -> {}, 
-    () -> !lidar.get(), 
-    new Subsystem[0]
-  ).andThen(new WaitCommand(0.3), new InstantCommand(rollers::neutralOutput));
-
-  public RobotContainer() {
-    linearActuator.setBoundsMicroseconds(2000, 0, 1500, 0, 1000);
+    public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    intakeTrigger.whileTrue(new StartEndCommand(() -> rollers.set(TalonSRXControlMode.PercentOutput, 0.5), () -> rollers.neutralOutput(), new Subsystem[0]));
-    expelTrigger.whileTrue(new StartEndCommand(() -> rollers.set(TalonSRXControlMode.PercentOutput, -0.5), () -> rollers.neutralOutput(), new Subsystem[0]));
-    autoTrigger.whileTrue(intakeCommand);
-    extendTrigger.onTrue(new InstantCommand(() -> linearActuator.set(1.0)));
-    contractTrigger.onTrue(new InstantCommand(() -> linearActuator.set(0.0)));
   }
 
   public Command getAutonomousCommand() {
