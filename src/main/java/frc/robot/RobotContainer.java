9848d9c5d1965bb04c0367f762adc763e549bcf9
Tori <ndtech260@gmail.com>
2024-03-09 13:08:59 -0500

    not forward

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -222,8 +222,8 @@ public class RobotContainer {
   }
 
   public void enablePhotonvisionPortForwarding() {
-    PortForwarder.add(5800, "10.50.0.37", 5800);
-    PortForwarder.add(5800, "10.50.0.11", 5800);
-    PortForwarder.add(5800, "photonvision.local", 5800);
+    // PortForwarder.add(5800, "10.50.0.37", 5800);
+    // PortForwarder.add(5800, "10.50.0.11", 5800);
+    // PortForwarder.add(5800, "photonvision.local", 5800);
   }
 }
